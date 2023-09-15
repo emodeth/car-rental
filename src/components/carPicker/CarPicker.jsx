bc00bef922cd3bc51b3bf27d26c62ba211d49c5e
emodeth <emodethistaken@gmail.com>
2023-09-15 23:32:54 +0300

    Car picker section's responsive desing and loader added

--- src/components/carPicker/CarPicker.jsx
+++ src/components/carPicker/CarPicker.jsx
@@ -4,18 +4,9 @@ import { useEffect, useState } from "react";
 
 function CarPicker() {
   const [activeCar, setActiveCar] = useState(1);
-  const {
-    img,
-    name,
-    price,
-    model,
-    mark,
-    year,
-    doors,
-    air,
-    transmission,
-    fuel,
-  } = CAR_DATA.at(activeCar)[0];
+  const [carLoad, setCarLoad] = useState(true);
+  const { price, model, mark, year, doors, air, transmission, fuel } =
+    CAR_DATA.at(activeCar)[0];
 
   useEffect(function () {
     const buttons = document.querySelectorAll(".car-picker__button");
@@ -33,7 +24,10 @@ function CarPicker() {
     <div className="car-picker">
       <div className="car-picker__cars">
         <button
-          onClick={() => setActiveCar(1)}
+          onClick={() => {
+            setActiveCar(1);
+            setCarLoad(true);
+          }}
           className="car-picker__button car-picker__button--active"
         >
           Audi A1 S-Line
@@ -41,6 +35,7 @@ function CarPicker() {
         <button
           onClick={() => {
             setActiveCar(0);
+            setCarLoad(true);
           }}
           className="car-picker__button"
         >
@@ -49,6 +44,7 @@ function CarPicker() {
         <button
           onClick={() => {
             setActiveCar(2);
+            setCarLoad(true);
           }}
           className="car-picker__button"
         >
@@ -57,6 +53,7 @@ function CarPicker() {
         <button
           onClick={() => {
             setActiveCar(3);
+            setCarLoad(true);
           }}
           className="car-picker__button"
         >
@@ -65,6 +62,7 @@ function CarPicker() {
         <button
           onClick={() => {
             setActiveCar(4);
+            setCarLoad(true);
           }}
           className="car-picker__button"
         >
@@ -73,49 +71,61 @@ function CarPicker() {
         <button
           onClick={() => {
             setActiveCar(5);
+            setCarLoad(true);
           }}
           className="car-picker__button"
         >
           VW Passat CC
         </button>
       </div>
-      <div className="car-picker__car-image-container">
-        <img src={img} alt={name} className="car-picker__car-image" />
-      </div>
-      <div className="car-picker__description">
-        <div className="car-picker__price">
-          <span>${price}</span> / rent per day
+      <div className="car-picker__car">
+        <div className="car-picker__car-image-container">
+          {carLoad && <span className="loader"></span>}
+          {CAR_DATA.at(activeCar).map((data) => (
+            <img
+              src={data.img}
+              alt={data.name}
+              className="car-picker__car-image"
+              style={{ display: carLoad ? "none" : "block" }}
+              onLoad={() => setCarLoad(false)}
+            />
+          ))}
         </div>
-        <div className="car-picker__table">
-          <div className="car-picker__col">
-            <span>Model</span>
-            <span>{model}</span>
+        <div className="car-picker__description">
+          <div className="car-picker__price">
+            <span>${price}</span> / rent per day
           </div>
-          <div className="car-picker__col">
-            <span>Mark</span>
-            <span>{mark}</span>
+          <div className="car-picker__table">
+            <div className="car-picker__col">
+              <span>Model</span>
+              <span>{model}</span>
+            </div>
+            <div className="car-picker__col">
+              <span>Mark</span>
+              <span>{mark}</span>
+            </div>
+            <div className="car-picker__col">
+              <span>Year</span>
+              <span>{year}</span>
+            </div>
+            <div className="car-picker__col">
+              <span>Doors</span>
+              <span>{doors}</span>
+            </div>
+            <div className="car-picker__col">
+              <span>AC</span>
+              <span>{air}</span>
+            </div>
+            <div className="car-picker__col">
+              <span>Transmission</span>
+              <span>{transmission}</span>
+            </div>
+            <div className="car-picker__col">
+              <span>Fuel</span>
+              <span>{fuel}</span>
+            </div>
+            <button className="cta-btn">Reserve Now</button>
           </div>
-          <div className="car-picker__col">
-            <span>Year</span>
-            <span>{year}</span>
-          </div>
-          <div className="car-picker__col">
-            <span>Doors</span>
-            <span>{doors}</span>
-          </div>
-          <div className="car-picker__col">
-            <span>AC</span>
-            <span>{air}</span>
-          </div>
-          <div className="car-picker__col">
-            <span>Transmission</span>
-            <span>{transmission}</span>
-          </div>
-          <div className="car-picker__col">
-            <span>Fuel</span>
-            <span>{fuel}</span>
-          </div>
-          <button className="cta-btn">Reserve Now</button>
         </div>
       </div>
     </div>
