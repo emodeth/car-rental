f5adf410c13fc649a1cc7ffbadd7d115050839ba
emodeth <emodethistaken@gmail.com>
2023-09-17 17:53:52 +0300

    Testimonials page completed

--- src/pages/TestimonialsPage.jsx
+++ src/pages/TestimonialsPage.jsx
@@ -1,10 +1,17 @@
 import PageHeader from "../components/pageHeader/PageHeader";
+import Testimonials from "../components/testimonials/Testimonials";
+import PageBanner from "../components/pageBanner/PageBanner";
+import Footer from "../components/footer/Footer";
+
 import "./testimonialsPage.css";
 
 function TestimonialsPage() {
   return (
     <>
       <PageHeader header="Testimonials" />
+      <Testimonials />
+      <PageBanner />
+      <Footer />
     </>
   );
 }
